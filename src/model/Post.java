6d22522c18f244b1f490865eb26c241326d2d5af
plusdojo2024-kameda <kameda-tsubasa-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-13 18:05:48 +0900

    java

--- src/model/Post.java
+++ src/model/Post.java
@@ -1,5 +1,56 @@
 package model;
+import java.io.Serializable;
+public class Post implements Serializable{
 
-public class Post {
+	private int post_id;
+	private int user_id;
+	private String title;
+	private String recommend;
+	private int interest;
+	private String created_at;
+	private String updated_at;
+
+	public int getPost_id() {
+		return post_id;
+	}
+	public void setPost_id(int post_id) {
+		this.post_id = post_id;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getRecommend() {
+		return recommend;
+	}
+	public void setRecommend(String recommend) {
+		this.recommend = recommend;
+	}
+	public int getInterest() {
+		return interest;
+	}
+	public void setInterest(int interest) {
+		this.interest = interest;
+	}
+	public String getCreated_at() {
+		return created_at;
+	}
+	public void setCreated_at(String created_at) {
+		this.created_at = created_at;
+	}
+	public String getUpdated_at() {
+		return updated_at;
+	}
+	public void setUpdated_at(String updated_at) {
+		this.updated_at = updated_at;
+	}
 
 }
--- src/model/Review.java
+++ src/model/Review.java
@@ -1,5 +1,64 @@
 package model;
+import java.io.Serializable;
+public class Review implements Serializable {
 
-public class Review {
+	private int review_id;
+	private int user_id;
+	private int contents_id;
+	private String title;
+	private String review;
+	private int good;
+	private String created_at;
+	private String updated_at;
+
+	public int getReview_id() {
+		return review_id;
+	}
+	public void setReview_id(int review_id) {
+		this.review_id = review_id;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public int getContents_id() {
+		return contents_id;
+	}
+	public void setContents_id(int contents_id) {
+		this.contents_id = contents_id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getReview() {
+		return review;
+	}
+	public void setReview(String review) {
+		this.review = review;
+	}
+	public int getGood() {
+		return good;
+	}
+	public void setGood(int good) {
+		this.good = good;
+	}
+	public String getCreated_at() {
+		return created_at;
+	}
+	public void setCreated_at(String created_at) {
+		this.created_at = created_at;
+	}
+	public String getUpdated_at() {
+		return updated_at;
+	}
+	public void setUpdated_at(String updated_at) {
+		this.updated_at = updated_at;
+	}
+
 
 }

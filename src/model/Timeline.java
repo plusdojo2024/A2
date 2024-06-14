9640bfe469a112fba8fcc735e0053469b591ec6f
plusdojo2024-seimiya <seimiya-miho-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-14 16:31:05 +0900

    common

--- src/model/Timeline.java
+++ src/model/Timeline.java
@@ -2,33 +2,9 @@ package model;
 import java.io.Serializable;
 
 public class Timeline implements Serializable {
-	private User user;					//Userクラス
-	private FavoriteUser favoriteUser;	//FavoriteUserクラス
-	private Review review;				//Reviewクラス
-	private Good good;					//Goodクラス
+	private int review_id;
+	private int user_id;
+	private int good;
+	private int user_id_writer;
 
-	public User getUser() {
-		return user;
-	}
-	public void setUser(User user) {
-		this.user = user;
-	}
-	public FavoriteUser getFavoriteUser() {
-		return favoriteUser;
-	}
-	public void setFavoriteUser(FavoriteUser favoriteUser) {
-		this.favoriteUser = favoriteUser;
-	}
-	public Review getReview() {
-		return review;
-	}
-	public void setReview(Review review) {
-		this.review = review;
-	}
-	public Good getGood() {
-		return good;
-	}
-	public void setGood(Good good) {
-		this.good = good;
-	}
 }

835ebed262c85d501e6bd1bac9937963ab7bee06
plusdojo2024-Maruyama-Manaho <maruyama-manaho-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-17 15:38:43 +0900

    sql文一覧更新

--- src/servlet/MyPageServlet.java
+++ src/servlet/MyPageServlet.java
@@ -1,5 +1,42 @@
 package servlet;
 
-public class MyPageServlet {
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.User;
+
+@WebServlet("/MyPageServlet")
+public class MyPageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	// もしもログインしていなかったらログインサーブレットにリダイレクトする
+    	HttpSession session = request.getSession();
+    	if (session.getAttribute("User") == null) {
+    		response.sendRedirect("/A2/LoginServlet");
+    		return;
+    	}
+
+    	//セッションスコープからユーザーIDを取ってくる
+    	User user = (User)session.getAttribute("user");
+    	String id = user.getUser_id();
+
+        //コレクション、ウィッシュリストの一覧を取得してセッションスコープに入れる
+
+        //
+
+
+
+    	// マイページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/myPage.jsp");
+		dispatcher.forward(request, response);
+    }
 }
+

8d1bfa10b3202c7143b602f81565857285e6ebc5
plusdojo2024-fujii <fujii-yuko-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-18 17:04:35 +0900

    更新

--- src/dao/PostDao.java
+++ src/dao/PostDao.java
@@ -1,20 +1,18 @@
 package dao;
 
-import model.Post;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Timestamp;
+
+import model.Post;
 
 public class PostDao {
 
-	public List<Post> postSelect(int postId ){
+	public boolean postInsert(Post post ){
 		Connection conn = null;
-    	List<Post> reviewList = new ArrayList<Post>();
+    	boolean result = false;
 
     	try {
     		// JDBCドライバを読み込む
@@ -23,13 +21,43 @@ public class PostDao {
     		// データベースに接続する
     		conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/wac", "sa", "");
 
+    		// Java側で現在のタイムスタンプを取得
+    		Timestamp createdAt = new Timestamp(System.currentTimeMillis());
+
     		// SQL文を準備する
-    		String sql = "";
+    		String sql = "INSERT INTO post(user_id_writer, title, recommend, created_at) VALUE(?, ?, ?, ?) ";
 
     		PreparedStatement pStmt = conn.prepareStatement(sql);
 
     		// SQL文を完成させる
-    		pStmt.setInt(1, postId);
-    	}
+    		pStmt.setInt(1, post.getUserId());
+    		pStmt.setString(2, post.getTitle());
+    		pStmt.setString(3, post.getRecommend());
+    		pStmt.setTimestamp(4, createdAt);
+
+    		// SQL文を実行する
+    		if (pStmt.executeUpdate() == 1) {
+    			result = true;
+    			}
+    		}
+    		catch (SQLException e) {
+    			e.printStackTrace();
+    		}
+    		catch (ClassNotFoundException e) {
+    			e.printStackTrace();
+    		}
+    		finally {
+    			// データベースを切断
+    			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+    	// 結果を返す
+		return result;
 	}
 }
--- src/servlet/HomeServlet.java
+++ src/servlet/HomeServlet.java
@@ -1,5 +1,60 @@
 package servlet;
 
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.ReviewDisplayDao;
+import dao.UserDao;
+import model.FavoriteUser;
+import model.Review;
+import model.ReviewDisplay;
+import model.User;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/HomeServlet")
+
 public class HomeServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("loginUser") == null) {
+			response.sendRedirect("/A2/LoginServlet");
+			return;
+		}
+
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("");
+
+		//セッションスコープからユーザIDを取ってくる
+		Review review = (Review)session.getAttribute("Review");
+		int user_id = review.getUserId();
+
+		//実績を取得する
+    	//DAOを生成
+		Review rDao = new Review();
+		//投稿したレビューの総数を取得する
+		int reviewCount = rDao.reviewCount(otherUserId);
+
+		//お気に入りユーザ検索
+		FavoriteUser fDao = new FavoriteUser();
+		boolean favoriteuser = fDao.
+
+
+
+		//マイページのフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/A2/jsp/home.jsp");
+		dispatcher.forward(request, response);
+	}
 
 }

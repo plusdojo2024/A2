f405fe594649de424c569b2b02487e60d9f5d70b
plusdojo2024-seimiya <seimiya-miho-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-18 13:35:02 +0900

    あ

--- src/dao/ReviewDao.java
+++ src/dao/ReviewDao.java
@@ -1,5 +1,21 @@
 package dao;
 
-public class ReviewDao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
 
+public class ReviewDao {
+	public List<Review> select(Review review) {
+		Connection conn = null;
+		ArrayList<Review> reviewList = new ArrayList<Review>();
+
+		try {
+			Class.forName("org.h2.Driver");
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/wac", "sa", "");
+			String sql =
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+		}
+	}
 }

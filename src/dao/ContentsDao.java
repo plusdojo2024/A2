e0177d71fa3d7b880afce1312594f350044f3ab8
plusdojo2024-matsui <matsui-ryunosuke-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-18 13:20:54 +0900

    DAO更新

--- src/dao/ContentsDao.java
+++ src/dao/ContentsDao.java
@@ -1,5 +1,65 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.SQLNonTransientException;
+
+import model.Contents;
+
 public class ContentsDao {
 
+//コンテンツ情報を登録し、結果を返す
+		public boolean contentsRegist(Contents contents) {
+			Connection conn = null;
+			boolean result = false;
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/wac", "sa", "");
+
+				// INSERTT文を準備する
+				String sql = "INSERT INTO contents(Title,Ruby,Image,Genre,Year,Creator,CreatedAt) VALUES (?,?,?,?,?,?,?)";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+				pStmt.setString(1, contents.getTitle());
+				pStmt.setString(2, contents.getRuby());
+				pStmt.setString(3, contents.getImage());
+				pStmt.setString(4, contents.getGenre());
+				pStmt.setString(5, contents.getYear());
+				pStmt.setString(6, contents.getCreator());
+				pStmt.setString(7, contents.getCreatedAt());
+
+				// INSERT文を実行し、登録に成功したらresultにtrueを入れる
+				if (pStmt.executeUpdate() == 1) {
+					result = true;
+				}
+			}
+			catch (SQLNonTransientException e) {
+				e.printStackTrace();
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+			// 結果を返す
+			return result;
+		}
 }
--- src/dao/FavoriteUserDao.java
+++ src/dao/FavoriteUserDao.java
@@ -1,5 +1,107 @@
 package dao;
 
-public class FavoriteUserDao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.SQLNonTransientException;
 
+public class FavoriteUserDao {
+//お気に入りユーザー情報を登録し、結果を返す
+		public boolean userRegist(int userIdFavorite, int userId) {
+			Connection conn = null;
+			boolean result = false;
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/wac", "sa", "");
+
+				// INSERTT文を準備する
+				String sql = "INSERT INTO favorite_user(user_id_favorite,user_id) VALUES (?,?)";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+				pStmt.setInt(1, userIdFavorite);
+				pStmt.setInt(2, userId);
+
+				// INSERT文を実行し、登録に成功したらresultにtrueを入れる
+				if (pStmt.executeUpdate() == 1) {
+					result = true;
+				}
+			}
+			catch (SQLNonTransientException e) {
+				e.printStackTrace();
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+			// 結果を返す
+			return result;
+		}
+
+
+
+		// 引数numberで指定されたお気に入りユーザー情報を削除し、成功したらtrueを返す
+		public boolean UserDelete(int userIdFavorite, int userId) {
+			Connection conn = null;
+			boolean result = false;
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/wac", "sa", "");
+
+				// SQL文を準備する
+				String sql = "DELETE FROM favorite_user(user_id_favorite,user_id) VALUES (?,?)";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				// SQL文を完成させる
+				pStmt.setInt(1, userIdFavorite);
+				pStmt.setInt(2, userId);
+
+				// SQL文を実行する
+				if (pStmt.executeUpdate() == 1) {
+					result = true;
+				}
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+			// 結果を返す
+			return result;
+		}
 }
+

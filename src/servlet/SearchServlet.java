d063eca038d55f05797aaa81617b95582b0b0cf3
plusdojo2024-tsutsui <tsutsui-shunta-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-19 14:25:07 +0900

    SearchServlet 完了

--- src/servlet/SearchServlet.java
+++ src/servlet/SearchServlet.java
@@ -1,5 +1,48 @@
 package servlet;
 
-public class SearchServlet {
+import java.io.IOException;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.ContentsDao;
+import model.Contents;
+
+
+@WebServlet("/SearchServlet")
+public class SearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("loginUser") == null) {
+			response.sendRedirect("/A2/LoginServlet");
+			return;
+		}
+
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String freeWord = request.getParameter("freeWord");
+
+
+		// 検索処理を行う
+		ContentsDao cDao = new ContentsDao();
+		List<Contents> contentsList = cDao.searchContents(freeWord);
+
+
+		// 検索結果をリクエストスコープに格納する
+		request.setAttribute("contentsList", contentsList);
+
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/searchResult.jsp");
+		dispatcher.forward(request, response);
+	}
 }

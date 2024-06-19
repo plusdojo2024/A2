c614d076093ac295869a7cef84858e28f8dd3197
plusdojo2024-Maruyama-Manaho <maruyama-manaho-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-19 10:20:42 +0900

    favoriteUserのビーンズ変更

--- src/model/FavoriteUser.java
+++ src/model/FavoriteUser.java
@@ -2,8 +2,10 @@ package model;
 import java.io.Serializable;
 
 public class FavoriteUser implements Serializable {
-	private int userId;			//ユーザID
-	private int userIdFavorite;	//お気に入りユーザID
+	private int userId;			//自分のユーザID
+	private int userIdFavorite;	//お気に入りユーザのユーザID
+	private String userNameFavorite;	//お気に入りユーザのユーザ名
+	private String iconFavorite;	//お気に入りユーザのアイコン画像
 	private String createdAt;	//登録日時
 	private String updateAt;	//更新日時
 
@@ -18,6 +20,18 @@ public class FavoriteUser implements Serializable {
 	}
 	public void setUserIdFavorite(int userIdFavorite) {
 		this.userIdFavorite = userIdFavorite;
+	}
+	public String getUserNameFavorite() {
+		return userNameFavorite;
+	}
+	public void setUserNameFavorite(String userNameFavorite) {
+		this.userNameFavorite = userNameFavorite;
+	}
+	public String getIconFavorite() {
+		return iconFavorite;
+	}
+	public void setIconFavorite(String iconFavorite) {
+		this.iconFavorite = iconFavorite;
 	}
 	public String getCreatedAt() {
 		return createdAt;

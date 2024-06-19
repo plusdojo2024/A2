f26a4d10643346dd0362ed15323a6e37e26f00e9
plusdojo2024-fujii <fujii-yuko-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-19 13:17:23 +0900

    更新

--- src/servlet/HomeServlet.java
+++ src/servlet/HomeServlet.java
@@ -1,23 +1,20 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import dao.GoodDao;
 import dao.ReviewDisplayDao;
-import dao.UserDao;
-import model.FavoriteUser;
-import model.Review;
+import dao.TimelineDao;
 import model.ReviewDisplay;
 import model.User;
-
-import javax.servlet.annotation.WebServlet;
 
 @WebServlet("/HomeServlet")
 
@@ -32,25 +29,30 @@ public class HomeServlet {
 			return;
 		}
 
-		// リクエストパラメータを取得する
-		request.setCharacterEncoding("UTF-8");
-		String id = request.getParameter("");
+		//ログインしているユーザIDを取得する
+		User loginUser = (User)session.getAttribute("loginUser");
+		int userId = loginUser.getUserId();
 
-		//セッションスコープからユーザIDを取ってくる
-		Review review = (Review)session.getAttribute("Review");
-		int user_id = review.getUserId();
-
-		//実績を取得する
-    	//DAOを生成
-		Review rDao = new Review();
-		//投稿したレビューの総数を取得する
-		int reviewCount = rDao.reviewCount(otherUserId);
-
-		//お気に入りユーザ検索
-		FavoriteUser fDao = new FavoriteUser();
-		boolean favoriteuser = fDao.
+		//1週間以内のお気に入りユーザのレビューを取得
+		TimelineDao tDao = new TimelineDao();
+		//レビューディスプレイDAOを生成
+    	ReviewDisplayDao rdDao = new ReviewDisplayDao();
+		List<ReviewDisplay> timeline = tDao.userSelect(userId);
+		//いいね数を取得する
+    	for(ReviewDisplay reviewDisplay: reviewList) {
+    		int reviewId = reviewDisplay.getReviewId();
+    		reviewDisplay.setGoodCount(rdDao.countGood(reviewId));
+    		reviewDisplay.setMyGood(rdDao.confirmGood(userId, reviewId));
+    	}
 
 
+		//全ユーザのいいね数の合計を取得し、上位５名のユーザ名とアイコンを取得する
+		GoodDao gDao = new GoodDao();
+		List<User> ranking = gDao.rankingSelect();
+
+		//リクエストスコープにセット
+		request.setAttribute("timeline", timeline);
+		request.setAttribute("ranking", ranking);
 
 		//マイページのフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/A2/jsp/home.jsp");

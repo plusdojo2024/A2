9c67a3da58a5a959380faf0225cda037adcdef72
plusdojo2024-kameda <kameda-tsubasa-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-19 14:57:55 +0900

    HTML

--- src/servlet/ApiMyContentsServlet.java
+++ src/servlet/ApiMyContentsServlet.java
@@ -15,17 +15,13 @@ import javax.servlet.http.HttpServletResponse;
 public class ApiMyContentsServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		doGet(request, response);

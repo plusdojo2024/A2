7cccf08842fdeb304be941ff09f6d3bafcdb6e33
plusdojo2024-Maruyama-Manaho <maruyama-manaho-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-20 19:09:26 +0900

    修正

--- src/servlet/HomeServlet.java
+++ src/servlet/HomeServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -39,7 +40,7 @@ public class HomeServlet {
     	ReviewDisplayDao rdDao = new ReviewDisplayDao();
 		List<ReviewDisplay> timeline = tDao.userSelect(userId);
 		//いいね数を取得する
-    	for(ReviewDisplay reviewDisplay: reviewList) {
+    	for(ReviewDisplay reviewDisplay: timeline) {
     		int reviewId = reviewDisplay.getReviewId();
     		reviewDisplay.setGoodCount(rdDao.countGood(reviewId));
     		reviewDisplay.setMyGood(rdDao.confirmGood(userId, reviewId));
@@ -47,12 +48,20 @@ public class HomeServlet {
 
 
 		//全ユーザのいいね数の合計を取得し、上位５名のユーザ名とアイコンを取得する
+    	//全ユーザのいいね数順のランキングを取得する
 		GoodDao gDao = new GoodDao();
 		List<User> ranking = gDao.rankingSelect();
 
+		//いいね数の多い順に並んだリストのうち、上位5名のみを選出したリストを作る
+		List<User> rankingTopFive = new ArrayList<User>();
+
+		for(int i = 0; i < 5; i++) {
+			rankingTopFive.add(ranking.get(i));
+		}
+
 		//リクエストスコープにセット
 		request.setAttribute("timeline", timeline);
-		request.setAttribute("ranking", ranking);
+		request.setAttribute("rankingTopFive", rankingTopFive);
 
 		//マイページのフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/A2/jsp/home.jsp");
